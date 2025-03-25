49485ca95f3b4704d94fb99f3d96e5203937ff0f
Karyne Santos <karynesantos0901@gmail.com>
2025-03-25 19:46:48 -0300

    fazendo a interligação do novo jogo adicionando design prontos criando as funções e adicionando a lógica do tiro da nave

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const ctx = canvas.getContext("2d");
 canvas.width = 720;
 canvas.height = 720;
 
+const containerJogo = document.querySelector('.container');
+const novoJogo = document.getElementById("novoJogo");
+
+
+novoJogo.addEventListener('click', (e) => {
+    e.preventDefault();
+    canvas.style.display = "block";
+    containerJogo.style.display = "none";
+});
+
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "visible") {
+        atualizarJogo();
+    }
+});
+
 const navePlayer = {
     x: canvas.width / 2 - 25,
     y: canvas.height - 60,
@@ -20,6 +36,9 @@ cenarioImagem.src = "/assets/img/cenario.png";
 var naveImagem = new Image();
 naveImagem.src = "/assets/img/valkyrie.png";
 
+var tiroImagem = new Image();
+tiroImagem.src = "/assets/img/bala.png";
+
 var inimigo1Imagem = new Image();
 inimigo1Imagem.src = "/assets/img/inimigo1.png";
 
@@ -35,6 +54,7 @@ const tempoEntreTiros = 100;
 
 let tirosInimigos = [];
 let inimigos = [];
+let ondaAtual = 1;
 let ultimoInimigo = 0;
 const numLinhas = 4;
 const numColunas = 7;
@@ -49,15 +69,27 @@ document.addEventListener("keydown", (e) => {
 
     if (e.key === " " && Date.now() - ultimoTiro > tempoEntreTiros) {
         ultimoTiro = Date.now();
-        tiros.push({
-            x: navePlayer.x + navePlayer.width / 2 - 1,
-            y: navePlayer.y,
-            width: 2,
-            height: 20,
-            dy: -5,
-            tempoVida: 0
-        });
+        
+        tiros.push(
+            {
+                x: navePlayer.x + navePlayer.width / 5 - 11,
+                y: navePlayer.y - 20,
+                width: 80,
+                height: 80,
+                dy: -5,
+                tempoVida: 0
+            },
+            {
+                x: navePlayer.x + (2 * navePlayer.width) / 5.5 - 13,
+                y: navePlayer.y - 20,
+                width: 80,
+                height: 80,
+                dy: -5,
+                tempoVida: 0
+            }
+        );
     }
+    
 });
 
 document.addEventListener("keyup", (e) => {
@@ -80,6 +112,7 @@ function criarInimigo() {
         ultimoInimigo = Date.now();
 
         let novosInimigos = [];
+        let tipoInimigo = ondaAtual;
 
         for (let linha = 0; linha < numLinhas; linha++) {
             for (let coluna = 0; coluna < numColunas; coluna++) {
@@ -90,7 +123,6 @@ function criarInimigo() {
                 
                 if (!existente) {
                     let posX = Math.random() * (canvas.width - 40);
-                    let tipoInimigo = Math.floor(Math.random() * 3) + 1;
 
                     novosInimigos.push({
                         x: posX,
@@ -110,6 +142,11 @@ function criarInimigo() {
         }
 
         inimigos = inimigos.concat(novosInimigos);
+        
+        if (inimigos.length === 0) {
+            ondaAtual++;
+            if (ondaAtual > 2) ondaAtual = 3;
+        }
     }
 }
 
@@ -136,16 +173,17 @@ function desenharJogo() {
         tiro.y += tiro.dy;
         tiro.tempoVida++;
 
-        if (tiro.y < 0 || tiro.tempoVida > 100) {
-            tiros.splice(index, 1);
+        if (tiroImagem.complete) {
+            ctx.drawImage(tiroImagem, tiro.x, tiro.y, tiro.width, tiro.height);
         } else {
             ctx.fillStyle = "red";
             ctx.fillRect(tiro.x, tiro.y, tiro.width, tiro.height);
         }
+        
     });
 
     criarInimigo();
-    inimigosAtirando();
+    // inimigosAtirando();
 
     inimigos.forEach((inimigo) => {
         if (!inimigo.alinhado) {
